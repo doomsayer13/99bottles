f1f510e60c5e80bb3454d852d44e36694a354be3
doomsayer13 <doomsayer13@gmail.com>
2020-05-27 23:05:19 +0200

    First test

---
+++ lib/bottles.rb
@@ -0,0 +1,10 @@
+class Bottles
+
+    def verse(number)
+        <<-VERSE
+#{number} bottles of beer on the wall, #{number} bottles of beer.
+Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+VERSE
+    end
+
+end
