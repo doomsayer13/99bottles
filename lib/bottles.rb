3603e3aad9a3320f24497efce60bd12095c62147
doomsayer13 <doomsayer13@gmail.com>
2020-06-17 08:11:14 +0200

    Refactoring based on Unit 3

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,30 +1,15 @@
 class Bottles
-# A Ruby beepitett "join" methodja tombokhoz, kivaltotta azt a bonyolult logikat amit
-# felepitettem, ahhoz hogy az elemeken vegigfutva osszefonjam oket es hogy az utolso
-# elemen kivul mindegyikhez fuzzek egy uj sor karaktert.
-# Jelentosen egyszerusodott a verses method.
-
-# A verse method-ban levo stringeket a jobb atlathatosag miatt kiemeltem private methodokba
-# melyek nevebol eredoen lathatjuk majd hogy melyik mit csinal
-# a verse method is olvashatobb lett.
-
   def verse(number)
     case number
-    when 0
-      last_verse
-    when 1
-      first_verse
-    when 2
-      second_verse
+    when 0 then no_more_verse
+    when 1 then last_verse
     else
-      nth_verse(number)
+      default_verse(number)
     end
   end
 
   def verses(from, to)
-    from.downto(to).map do |number|
-      verse(number)
-    end.join("\n")
+    from.downto(to).map { |verse_number| verse(verse_number) }.join("\n")
   end
 
   def song
@@ -33,31 +18,33 @@ class Bottles
 
   private
 
+  def no_more_verse
+    "No more bottles of beer on the wall, " +
+    "no more bottles of beer.\n" +
+    "Go to the store and buy some more, " +
+    "99 bottles of beer on the wall.\n"
+  end
+
   def last_verse
-    <<-VERSE
-No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-VERSE
+    "1 bottle of beer on the wall, " +
+    "1 bottle of beer.\n" +
+    "Take it down and pass it around, " +
+    "no more bottles of beer on the wall.\n"
   end
 
-  def first_verse
-    <<-VERSE
-1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-VERSE
+  def default_verse(n)
+    "#{n} bottles of beer on the wall, " +
+    "#{n} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{n-1} #{container(n-1)} of beer on the wall.\n"
   end
 
-  def second_verse
-    <<-VERSE
-2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-VERSE
-  end
-
-  def nth_verse(n)
-    <<-VERSE
-#{n} bottles of beer on the wall, #{n} bottles of beer.
-Take one down and pass it around, #{n - 1} bottles of beer on the wall.
-VERSE
+  # six-pack is still missing from the code
+  def container(n)
+    if n == 1
+      "bottle"
+    else
+      "bottles"
+    end
   end
 end
